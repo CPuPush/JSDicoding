56ab7c7b79a7eaa312af50b566936bc8db75726b
CPuPush <fpakpahan44@gmail.com>
2022-07-16 16:04:30 +0700

    adding

--- 9JavaSciptTesting/LatihanTesting/__test__/max.test.js
+++ 9JavaSciptTesting/LatihanTesting/__test__/max.test.js
@@ -1,7 +1,8 @@
+const {findMax} = require('../max')
 test('should return the max number between 3 arguments', () => {
-    expect(findMax(1, 2, 3)).toEqual(3);
-    expect(findMax(5, 1, 2)).toEqual(5);
-    expect(findMax(102, 404, 48)).toEqual(404);
-    expect(findMax(-1, 0, -1)).toEqual(0);
-    expect(findMax(2, 2, 2)).toEqual(2);
+    expect(findMax(1, 2, 3)).toBe(3);
+    expect(findMax(5, 1, 2)).toBe(5);
+    expect(findMax(102, 404, 48)).toBe(404);
+    expect(findMax(-1, 0, -1)).toBe(0);
+    expect(findMax(2, 2, 2)).toBe(2);
 })
--- 9JavaSciptTesting/LatihanTesting/max.js
+++ 9JavaSciptTesting/LatihanTesting/max.js
@@ -8,3 +8,4 @@ function findMax(a, b, c) {
         return c;
     }
 }
+module.exports = {findMax};
